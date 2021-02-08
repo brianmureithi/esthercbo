7bf4644bb9c7ef25c3a039335b29fcfac2285deb
brian mureithi <brianmurithi65@gmail.com>
2021-02-08 18:33:00 +0300

    added hero sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/navigation/Navbar';
+import Home from './components/Pages/Home'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <Router>
     <Navbar/>
     <Switch>
-      <Route path='' exact />
+      <Route path='' exact component={Home} />
     </Switch>
     </Router>
  
